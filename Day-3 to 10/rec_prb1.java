a90622d16e5694a52faddc7a87597dd7abb4f7e3
itsSnehaM <sneha2020mukherjee@gmail.com>
2022-01-21 23:32:35 +0530

    uploaded prb 1

---
+++ Day-3 to 10/rec_prb1.java
@@ -0,0 +1,26 @@
+class rec_prb1 {
+    static void reverseArray(int ar[], int start, int end)
+    {
+      int temp;
+      if (start >= end)
+        return;
+      temp = ar[start];
+      ar[start] = ar[end];
+      ar[end] = temp;
+      reverseArray(ar, start + 1, end - 1);
+    }
+    static void printArray(int ar[], int size)
+    {
+      for (int i = 0; i < size; i++)
+        System.out.print(ar[i] + " ");
+      System.out.println("");
+    }
+    public static void main (String[] args)
+    {
+      int arr[] = {1, 2, 3, 4, 5, 6};
+      printArray(arr, 6);
+      reverseArray(arr, 0, 5);
+      // System.out.println("Reversed array is ");
+      printArray(arr, 6);
+    }
+  }
